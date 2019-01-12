93a219fe524efb77ce2c381b04921a44abe4b3e7
Do Tuan Anh <dotuananh.dta@gmail.com>
2019-01-12 19:46:35 +0700

    feature complete

--- src/Pomodoro.js
+++ src/Pomodoro.js
@@ -1,29 +1,33 @@
-import React  from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronCircleUp, faChevronCircleDown, faClock, faPlayCircle, faPauseCircle, faSyncAlt } from '@fortawesome/free-solid-svg-icons'
 import { connect } from "react-redux";
 import { increaseBreak, decreaseBreak, increaseSession, decreaseSession, reset, startCountDown } from "./actions";
+import alarmSound from './alarm.mp3'
 
 library.add(faChevronCircleUp, faChevronCircleDown, faClock, faPlayCircle, faPauseCircle, faSyncAlt);
 
-const Pomodoro = ({ breakLength, sessionLength, timeLeft, isRunning, increaseBreak, decreaseBreak,
-  increaseSession, decreaseSession, togglePlay, reset }) => (
-    <div className="wrappper center">
-      <div className="center center-row">
-        <FontAwesomeIcon icon="clock" className="icon" id="app-icon" />
-        <h1>Pomodoro Clock</h1>
+class Pomodoro extends Component {
+  render() {
+    const { breakLength, sessionLength, timeLeft, isRunning, isInBreak, increaseBreak, decreaseBreak, increaseSession, decreaseSession, togglePlay, reset } = this.props;
+    return (
+      <div className="wrappper center" >
+        <div className="center center-row">
+          <FontAwesomeIcon icon="clock" className="icon" id="app-icon" />
+          <h1>Pomodoro Clock</h1>
+        </div>
+        <div className="center center-row">
+          <Controller type="break" value={breakLength} onIncrease={increaseBreak} onDecrease={decreaseBreak} />
+          <Controller type="session" value={sessionLength} onIncrease={increaseSession} onDecrease={decreaseSession} />
+        </div>
+        <Clock time={timeLeft} isInBreak={isInBreak} />
+        <Buttons isRunning={isRunning} togglePlay={togglePlay} reset={reset} />
       </div>
-      <div className="center center-row">
-        <Controller name="Break Length" id="break-label" value={breakLength} onIncrease={increaseBreak} onDecrease={decreaseBreak}/>
-        <Controller name="Session Length" id="session-label" value={sessionLength} onIncrease={increaseSession} onDecrease={decreaseSession}/>
-      </div>
-      <Clock time={timeLeft} />
-      <Buttons isRunning={isRunning} togglePlay={togglePlay} reset={reset}/>
-    </div>
-  );
-
+    );
+  }
+}
 
 const mapStateToProps = state => ({ ...state });
 const mapDispatchToProps = dispatch => (
@@ -39,28 +43,63 @@ const mapDispatchToProps = dispatch => (
 
 export default connect(mapStateToProps, mapDispatchToProps)(Pomodoro);
 
-const Controller = ({ name, id, value, onIncrease, onDecrease }) => (
+const Controller = ({ type, value, onIncrease, onDecrease }) => (
   <div className="center controller">
-    <div id={id}>{name}</div>
+    <div id={type + "-label"}>{capitalizeFirstLetter(type) + " Length"}</div>
     <div className="center center-row">
-      <FontAwesomeIcon icon="chevron-circle-up" className="icon" onClick={onIncrease}/>
-      {value}
-      <FontAwesomeIcon icon="chevron-circle-down" className="icon" onClick={onDecrease}/>
+      <div onClick={onIncrease} id={type + "-increment"}> {/* The test only works with click on div */}
+        <FontAwesomeIcon icon="chevron-circle-up" className="icon" />
+      </div>
+      <div id={type + "-length"}>{value}</div>
+      <div onClick={onDecrease} id={type + "-decrement"} >
+        <FontAwesomeIcon icon="chevron-circle-down" className="icon" />
+      </div>
     </div>
   </div>
 )
 
-const Clock = ({ time }) => (
-  <div id="clock" className="center">
-    <div id="timer-label">Session</div>
-    <div>{time}</div>
-  </div>
-)
+const Clock = ({ time, isInBreak }) => {
+  let minutes = Math.floor(time / 60);
+  minutes = minutes >= 10 ? minutes : '0' + minutes;
+  let seconds = time % 60;
+  seconds = seconds >= 10 ? seconds : '0' + seconds;
 
-const Buttons = ({ isRunning, togglePlay, reset }) => (
-  <div className="center center-row controller">
-    <FontAwesomeIcon icon={isRunning ? "pause-circle" : "play-circle"} className="icon" onClick={togglePlay}/>
-    <FontAwesomeIcon icon="sync-alt" className="icon" onClick={reset}/>
-  </div>
-)
+  if (time === 0) {
+    document.getElementById('beep').play();
+  }
 
+  return (
+    <div id="clock" className="center">
+      <div id="timer-label">{isInBreak ? "It's break time!" : "Session"}</div>
+      <div id="time-left">{minutes}:{seconds}</div>
+      <audio src={alarmSound} id="beep"></audio>
+    </div>
+  );
+}
+
+const Buttons = ({ isRunning, togglePlay, reset }) => {
+  // if (time === 0) {
+  //   document.getElementById('beep').play();
+  // }
+  function onReset() {
+    const audio = document.getElementById('beep');
+    audio.pause();
+    audio.currentTime = 0;
+    reset()
+  }
+
+  return (
+    <div className="center center-row controller">
+      <div onClick={togglePlay} id="start_stop" >
+        <FontAwesomeIcon icon={isRunning ? "pause-circle" : "play-circle"} className="icon" />
+      </div>
+      <div onClick={onReset} id="reset" >
+        <FontAwesomeIcon icon="sync-alt" className="icon" />
+      </div>
+    </div>
+  );
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
--- src/reducer.js
+++ src/reducer.js
@@ -1,21 +1,45 @@
 import * as Action from './actions';
 
 export function reducer(state = initialState, action) {
+  let sessionLength = state.sessionLength;
+  let breakLength = state.breakLength;
+
   switch (action.type) {
     case Action.INCREASE_BREAK:
-      return Object.assign({}, state, { breakLength: state.breakLength + 1 });
+      if (state.isRunning) return state;
+      if (breakLength < 60) breakLength++;
+      return Object.assign({}, state, { breakLength });
+
     case Action.DECREASE_BREAK:
-      return Object.assign({}, state, { breakLength: state.breakLength - 1 });
+      if (state.isRunning) return state;
+      if (breakLength > 1) breakLength--;
+      return Object.assign({}, state, { breakLength });
+
     case Action.INCREASE_SESSION:
-      return Object.assign({}, state, { sessionLength: state.sessionLength + 1 });
+      if (state.isRunning) return state;
+      if (sessionLength < 60) sessionLength++;
+      return Object.assign({}, state, { sessionLength, timeLeft: sessionLength * 60 });
+
     case Action.DECREASE_SESSION:
-      return Object.assign({}, state, { sessionLength: state.sessionLength - 1 });
+      if (state.isRunning) return state;
+      if (sessionLength > 1) sessionLength--;
+      return Object.assign({}, state, { sessionLength, timeLeft: sessionLength * 60 });
+
     case Action.TOGGLE_PLAY:
       return Object.assign({}, state, { isRunning: !state.isRunning });
+
     case Action.RESET:
       return initialState;
+
     case Action.COUNT_DOWN:
-      return Object.assign({}, state, { timeLeft: state.timeLeft - 1 });
+      let timeLeft = state.timeLeft - 1;
+      let isInBreak = state.isInBreak;
+      if (timeLeft < 0) {
+        isInBreak = !isInBreak;
+        timeLeft = isInBreak ? state.breakLength * 60 : state.sessionLength * 60;
+      }
+      return Object.assign({}, state, { timeLeft, isInBreak });
+
     default:
       return state;
   }
@@ -25,5 +49,6 @@ const initialState = {
   breakLength: 5,//in minutes
   sessionLength: 25,//in minutes
   timeLeft: 25 * 60,//in seconds
+  isInBreak: false,
   isRunning: false
 };
