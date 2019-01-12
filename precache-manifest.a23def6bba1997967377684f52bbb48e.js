f9c8d167b0159eaf2d5581ef1c5b413dcdf51d5d
Do Tuan Anh <dotuananh.dta@gmail.com>
2019-01-12 21:11:02 +0700

    Updates

---
+++ precache-manifest.a23def6bba1997967377684f52bbb48e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f154994f9c43e0768a5517b3a9c50f6",
+    "url": "/fcc10-pomodoro-clock/static/media/alarm.2f154994.mp3"
+  },
+  {
+    "revision": "2b56a4a57b5df92af648",
+    "url": "/fcc10-pomodoro-clock/static/js/runtime~main.2b56a4a5.js"
+  },
+  {
+    "revision": "008b62586a6e6b477c7c",
+    "url": "/fcc10-pomodoro-clock/static/js/main.008b6258.chunk.js"
+  },
+  {
+    "revision": "2061697ee7a3b1b7417f",
+    "url": "/fcc10-pomodoro-clock/static/js/1.2061697e.chunk.js"
+  },
+  {
+    "revision": "008b62586a6e6b477c7c",
+    "url": "/fcc10-pomodoro-clock/static/css/main.779e496a.chunk.css"
+  },
+  {
+    "revision": "15370c50d5f32e94761f5d9d3a3acb8e",
+    "url": "/fcc10-pomodoro-clock/index.html"
+  }
+];
